1c06d4b1772dc9afc23b9ee499ca0c4b4b8b8046
Teo Oriol <teo.oriol@epitech.eu>
2020-09-19 19:19:19 +0200

    OrganizationModel

---
+++ GAME_ADS_STUDIO_API/Models/Organization/OrganizationModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GAME_ADS_STUDIO_API.Models.Organization
+{
+    public class OrganizationModel
+    {
+        public uint Id { get; set; }
+        public string PublicEmail { get; set; }
+        public string PrivateEmail { get; set; }
+        public string Localization { get; set; }
+        public string LogoUrl { get; set; }
+        public string WebsiteUrl { get; set; }
+        public string Type { get; set; }
+        public string State { get; set; }
+        public string DefaultAuthorization { get; set; }
+        public DateTimeOffset CreationDate { get; set; }
+        public DateTimeOffset ModificationDate { get; set; }
+    }
+}
