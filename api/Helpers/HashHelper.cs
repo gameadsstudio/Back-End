5b6875068109de7a5c40e8a968565046dabf5a86
Teo Oriol <teo.oriol@epitech.eu>
2021-04-24 07:11:22 +0200

    feature/users -> added HashHelper

---
+++ api/Helpers/HashHelper.cs
@@ -0,0 +1,20 @@
+namespace api.Helpers
+{
+    public static class HashHelper
+    {
+        private static string GetRandomSalt()
+        {
+            return BCrypt.Net.BCrypt.GenerateSalt(12);
+        }
+        
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, GetRandomSalt());
+        }
+        
+        public static bool ValidatePassword(string password, string correctHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, correctHash);
+        }
+    }
+}
