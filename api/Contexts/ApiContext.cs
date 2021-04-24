fa440d84b0cbe94995d65b3f490baf408a2d9da9
Teo Oriol <teo.oriol@epitech.eu>
2021-04-24 07:11:45 +0200

    feature/users -> added automatic date management on models

--- api/Contexts/ApiContext.cs
+++ api/Contexts/ApiContext.cs
@@ -1,4 +1,6 @@
-﻿using api.Models.Campaign;
+﻿using System;
+using System.Linq;
+using api.Models.Campaign;
 using api.Models.Game;
 using api.Models.Organization;
 using api.Models.User;
@@ -20,6 +22,20 @@ namespace api.Contexts
         public DbSet<GameModel> Game { get; set; }
         public DbSet<OrganizationModel> Organization { get; set; }
 
+        public override int SaveChanges()
+        {
+            DateTime saveTime = DateTime.Now;
+            foreach (var entry in this.ChangeTracker.Entries()
+                .Where(e => e.State == (EntityState) EntityState.Added))
+            {
+                if (entry.Property("DateCreation").CurrentValue == null || (DateTimeOffset)entry.Property("DateCreation").CurrentValue == DateTime.MinValue)
+                {
+                    entry.Property("DateCreation").CurrentValue = (DateTimeOffset)saveTime;
+                }
+                entry.Property("DateUpdate").CurrentValue = (DateTimeOffset)saveTime;
+            }
+            return base.SaveChanges(); 
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<UserModel>(entity =>
