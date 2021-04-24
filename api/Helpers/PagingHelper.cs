344ef90cd8d07dedc806727b0c0e5f27e26c51ed
Teo Oriol <teo.oriol@epitech.eu>
2021-04-24 07:11:55 +0200

    feature/users -> added paging helper

---
+++ api/Helpers/PagingHelper.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace api.Helpers
+{
+    public class PagingDto
+    {
+        public int page { get; set; } = 1;
+        public int pageSize { get; set; } = 50;
+    }
+    
+    public static class PagingHelper
+    {
+        public static PagingDto Check(PagingDto paging)
+        {
+            if (paging.page < 1)
+            {
+                paging.page = 0;
+            }
+            if (paging.pageSize < 1 || paging.pageSize > 50)
+            {
+                paging.pageSize = 50;
+            }
+            return paging;
+        }
+    }
+}
