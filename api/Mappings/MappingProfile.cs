7bffe542901966486cc08a7aabec0697587edbdb
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-04-25 18:39:10 +0200

    feature/tags -> TagBusinessLogic skeleton + registering in startup + add mapping

--- api/Mappings/MappingProfile.cs
+++ api/Mappings/MappingProfile.cs
@@ -1,3 +1,4 @@
+using api.Models.Tag;
 using api.Models.User;
 using AutoMapper;
 
@@ -5,10 +6,15 @@ namespace api.Mappings
 {
     public class MappingProfile : Profile {
         public MappingProfile() {
+            // User
             CreateMap<UserModel, UserCreationModel>().ReverseMap();
             CreateMap<UserModel, UserUpdateModel>().ReverseMap().ForAllMembers(opts => opts.Condition((_, _, srcMember) => srcMember != null));
             CreateMap<UserModel, UserPrivateModel>().ReverseMap();
             CreateMap<UserModel, UserPublicModel>().ReverseMap();
+
+            // Tag
+            CreateMap<TagModel, TagCreationModel>().ReverseMap();
+            CreateMap<TagModel, TagUpdateModel>().ReverseMap().ForAllMembers(opts => opts.Condition((_, _, srcMember) => srcMember != null));
         }
     }
 }
