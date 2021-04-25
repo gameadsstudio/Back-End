dd5687f82edb7393d73646a8d7ebd172c12d092b
Teo Oriol <teo.oriol@epitech.eu>
2021-04-25 06:26:35 +0200

    feature/users -> fixed jwts were still valid even if user didn't exist anymore

--- api/Startup.cs
+++ api/Startup.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Linq;
+using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 using api.Configuration;
 using api.Contexts;
 using api.Mappings;
+using api.Middlewares;
 using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -28,34 +32,49 @@ namespace api
         public void ConfigureServices(IServiceCollection services)
         {
             var key = Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("GAS_SECRET")!);
-            
             services.Configure<AppSettings>(Configuration.GetSection("Application"));
             services.AddControllers();
             services.AddDbContext<ApiContext>(p =>
                 p.UseNpgsql(
-                    $"Host={Environment.GetEnvironmentVariable("GAS_DATABASE_SERVER")};Port=5432;Database={Environment.GetEnvironmentVariable("GAS_POSTGRES_DB")};Username={Environment.GetEnvironmentVariable("GAS_POSTGRES_USER")};Password={Environment.GetEnvironmentVariable("GAS_POSTGRES_PASSWORD")};")
-                    .UseSnakeCaseNamingConvention()
-                    );
+                        $"Host={Environment.GetEnvironmentVariable("GAS_DATABASE_SERVER")};Port=5432;Database={Environment.GetEnvironmentVariable("GAS_POSTGRES_DB")};Username={Environment.GetEnvironmentVariable("GAS_POSTGRES_USER")};Password={Environment.GetEnvironmentVariable("GAS_POSTGRES_PASSWORD")};")
+                    .UseSnakeCaseNamingConvention());
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Game Ads Studio API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Game Ads Studio API", Version = "v1"});
             });
-            services.AddSingleton(new MapperConfiguration(mc =>
-            {
-                mc.AddProfile(new MappingProfile());
-            }).CreateMapper());
+            services.AddSingleton(
+                new MapperConfiguration(mc => { mc.AddProfile(new MappingProfile()); }).CreateMapper());
             
-            services.AddAuthentication(x => {
+            services.AddAuthentication(x =>
+            {
                 x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(x => {
+            }).AddJwtBearer(x =>
+            {
                 x.RequireHttpsMetadata = false;
                 x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
                     ValidateIssuerSigningKey = true,
                     IssuerSigningKey = new SymmetricSecurityKey(key),
                     ValidateIssuer = false,
                     ValidateAudience = false
+                };
+                x.Events = new JwtBearerEvents()
+                {
+                    OnTokenValidated = context =>
+                    {
+                        var dbcontext = context.HttpContext.RequestServices.GetRequiredService<ApiContext>();
+                        var principal = context.Principal;
+
+                        var claim = principal?.Claims.FirstOrDefault(p => p.Type == ClaimTypes.NameIdentifier);
+                        if (claim == null || dbcontext.User.SingleOrDefault(a => a.Id == new Guid(claim.Value)) == null)
+                        {
+                            context.Fail("Invalid token");
+                        }
+                        return Task.CompletedTask;
+                    },
+                    
                 };
             });
         }
@@ -71,17 +90,12 @@ namespace api
 
             // Auto migrate database on startup
             context.Database.Migrate();
-
-            app.UseCors(builder => builder
-                .AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader());
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseHttpsRedirection();
             app.UseRouting();
-
             app.UseAuthentication();
             app.UseAuthorization();
-
+            app.UseMiddleware<ErrorHandlingMiddleware>();
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
             app.UseSwagger();
             app.UseSwaggerUI(c =>
