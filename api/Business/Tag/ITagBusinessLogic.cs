fe90f03c4c18cadb846d08fcab3604669f1efad6
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-04-25 18:19:31 +0200

    feature/tags -> TagController

--- api/Business/Tag/ITagBusinessLogic.cs
+++ api/Business/Tag/ITagBusinessLogic.cs
@@ -7,8 +7,8 @@ namespace api.Business.Tag
     public interface ITagBusinessLogic
     {
         TagModel GetTagById(string id);
-        (int page, int pageSize, int maxPage, TagModel[] tags) GetTags(PagingDto paging);
-        TagModel AddNewTag(TagCreationModel newTag);
+        (int page, int pageSize, int maxPage, TagModel[] tags) GetTags(PagingDto paging, string name, string description);
+        TagModel AddNewTag(TagCreationModel newTag, Claim currentUser);
         TagModel UpdateTagById(string id, TagUpdateModel updatedTag, Claim currentUser);
         void DeleteTagById(string id, Claim currentUser);
     }
---
+++ api/Controllers/Tag/TagController.cs
@@ -0,0 +1,71 @@
+using System.Linq;
+using System.Security.Claims;
+using api.Business.Tag;
+using api.Helpers;
+using api.Models.Tag;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers.Tag
+{
+    [Route("/v1/tags")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private readonly ITagBusinessLogic _business;
+
+        public TagController(ITagBusinessLogic business)
+        {
+            _business = business;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            return Ok(_business.GetTagById(id));
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult GetAll(
+            [FromQuery] PagingDto paging,
+            [FromQuery] string name,
+            [FromQuery] string description)
+        {
+            var result = _business.GetTags(paging, name, description);
+
+            return Ok(new
+            {
+                status = 200,
+                page = result.Item1,
+                pagesize = result.Item2,
+                maxPage = result.Item3,
+                tags = result.Item4
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromForm] TagCreationModel newTag)
+        {
+            var currentUser = User.Claims.FirstOrDefault(p => p.Type == ClaimTypes.NameIdentifier);
+            var success = _business.AddNewTag(newTag, currentUser);
+            return Created("tag", success);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(string id, [FromForm] TagUpdateModel newTag)
+        {
+            var currentUser = User.Claims.FirstOrDefault(p => p.Type == ClaimTypes.NameIdentifier);
+            return Ok(_business.UpdateTagById(id, newTag, currentUser));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var currentUser = User.Claims.FirstOrDefault(p => p.Type == ClaimTypes.NameIdentifier);
+            _business.DeleteTagById(id, currentUser);
+            return Ok();
+        }
+    }
+}
