33649023537eedb7e98f8eb00adcf1b84ffafb67
Teo Oriol <teo.oriol@epitech.eu>
2021-04-25 04:55:43 +0200

    feature/users -> add jwt auth in startup.cs

--- api/Startup.cs
+++ api/Startup.cs
@@ -1,14 +1,17 @@
 using System;
+using System.Text;
 using api.Configuration;
 using api.Contexts;
 using api.Mappings;
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace api
@@ -24,6 +27,8 @@ namespace api
 
         public void ConfigureServices(IServiceCollection services)
         {
+            var key = Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("GAS_SECRET")!);
+            
             services.Configure<AppSettings>(Configuration.GetSection("Application"));
             services.AddControllers();
             services.AddDbContext<ApiContext>(p =>
@@ -39,6 +44,20 @@ namespace api
             {
                 mc.AddProfile(new MappingProfile());
             }).CreateMapper());
+            
+            services.AddAuthentication(x => {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x => {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApiContext context)
