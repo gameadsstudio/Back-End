e8496edda6fc5d56e90f351a5d727e196ccb4ec5
Teo Oriol <teo.oriol@epitech.eu>
2021-04-25 13:58:30 +0200

    feature/users -> code cleanup

--- api/Models/User/UserUpdateModel.cs
+++ api/Models/User/UserUpdateModel.cs
@@ -7,12 +7,19 @@ namespace api.Models.User
         [EmailAddress] public string Email { get; set; }
 
         public string Username { get; set; }
+        
         public string FirstName { get; set; }
+        
         public string LastName { get; set; }
+        
         public string Password { get; set; }
+        
         public string Alias { get; set; }
+        
         public string Phone { get; set; }
+        
         public string Level { get; set; }
+        
         public string Status { get; set; }
     }
 }
