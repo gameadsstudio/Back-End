44b66e2cd1dc782e687a4c204fbba2d8ce1ff467
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-04-26 10:55:03 +0200

    feature/tags -> TagsBusinessLogic + updated repository; not tested

--- api/Business/Tag/TagBusinessLogic.cs
+++ api/Business/Tag/TagBusinessLogic.cs
@@ -1,50 +1,77 @@
+using System;
+using System.Net;
 using System.Security.Claims;
-using api.Configuration;
 using api.Contexts;
+using api.Errors;
 using api.Helpers;
 using api.Models.Tag;
 using api.Repositories.Tag;
 using AutoMapper;
-using Microsoft.Extensions.Options;
 
 namespace api.Business.Tag
 {
     public class TagBusinessLogic : ITagBusinessLogic
     {
-        private readonly AppSettings _appSettings;
         private readonly IMapper _mapper;
         private readonly ITagRepository _repository;
 
-        public TagBusinessLogic(ApiContext context, IOptions<AppSettings> appSettings, IMapper mapper)
+        public TagBusinessLogic(ApiContext context, IMapper mapper)
         {
             _repository = new TagRepository(context);
-            _appSettings = appSettings.Value;
             _mapper = mapper;
         }
 
         public TagModel GetTagById(string id)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                return _repository.GetTagById(Guid.Parse(id)) ??
+                       throw new ApiError(HttpStatusCode.NotFound, $"Could not find tag with Id: {id}");
+            }
+            catch (FormatException e)
+            {
+                throw new ApiError(HttpStatusCode.BadRequest, e.Message);
+            }
         }
 
-        public (int page, int pageSize, int maxPage, TagModel[] tags) GetTags(PagingDto paging, string name, string description)
+        public (int page, int pageSize, int maxPage, TagModel[] tags) GetTags(PagingDto paging, string name,
+            string description)
         {
-            throw new System.NotImplementedException();
+            paging = PagingHelper.Check(paging);
+            var (tags, maxPage) = _repository.SearchTagsByNameOrDescription((paging.Page - 1) * paging.PageSize,
+                paging.PageSize, name, description);
+            return (paging.Page, paging.PageSize, (maxPage / paging.PageSize + 1), tags);
         }
 
         public TagModel AddNewTag(TagCreationModel newTag, Claim currentUser)
         {
-            throw new System.NotImplementedException();
+            // Todo: Check if user is admin
+
+            var tag = _mapper.Map(newTag, new TagModel());
+            return _repository.AddNewTag(tag);
         }
 
         public TagModel UpdateTagById(string id, TagUpdateModel updatedTag, Claim currentUser)
         {
-            throw new System.NotImplementedException();
+            // Todo: Check if user is admin
+
+            var tag = GetTagById(id);
+
+            if (!string.IsNullOrEmpty(updatedTag.Name) && _repository.GetTagByName(updatedTag.Name) != null)
+            {
+                throw new ApiError(HttpStatusCode.Conflict,
+                    $"Tag with name: {updatedTag.Name} already exists");
+            }
+
+            tag = _mapper.Map(updatedTag, tag);
+            return _repository.UpdateTag(tag);
         }
 
         public void DeleteTagById(string id, Claim currentUser)
         {
-            throw new System.NotImplementedException();
+            // Todo: Check if user is admin
+            var tag = GetTagById(id);
+            _repository.DeleteTag(tag);
         }
     }
 }
--- api/Repositories/Tag/ITagRepository.cs
+++ api/Repositories/Tag/ITagRepository.cs
@@ -8,7 +8,9 @@ namespace api.Repositories.Tag
         TagModel AddNewTag(TagModel tag);
         TagModel GetTagById(Guid id);
         TagModel GetTagByName(string name);
-        TagModel[] GetTags(int offset, int limit);
+        (TagModel[], int) GetTags(int offset, int limit);
+        public (TagModel[], int) SearchTagsByNameAndDescription(int offset, int limit, string name, string description);
+        public (TagModel[], int) SearchTagsByNameOrDescription(int offset, int limit, string name, string description);
         TagModel UpdateTag(TagModel updatedTag);
         int DeleteTag(TagModel tag);
     }
