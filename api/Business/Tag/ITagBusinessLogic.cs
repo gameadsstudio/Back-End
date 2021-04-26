24d8894632096900e5dc739b08f73e68f3aac992
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-04-26 17:11:10 +0200

    feature/tags -> Add the option to avoid paging if needed on Get tags

--- api/Business/Tag/ITagBusinessLogic.cs
+++ api/Business/Tag/ITagBusinessLogic.cs
@@ -7,7 +7,8 @@ namespace api.Business.Tag
     public interface ITagBusinessLogic
     {
         TagModel GetTagById(string id);
-        (int page, int pageSize, int maxPage, TagModel[] tags) GetTags(PagingDto paging, string name, string description);
+        (int page, int pageSize, int maxPage, TagModel[] tags) GetTags(PagingDto paging, string name,
+            string description, bool noPaging);
         TagModel AddNewTag(TagCreationModel newTag, Claim currentUser);
         TagModel UpdateTagById(string id, TagUpdateModel updatedTag, Claim currentUser);
         void DeleteTagById(string id, Claim currentUser);
--- api/Repositories/Tag/ITagRepository.cs
+++ api/Repositories/Tag/ITagRepository.cs
@@ -8,6 +8,8 @@ namespace api.Repositories.Tag
         TagModel AddNewTag(TagModel tag);
         TagModel GetTagById(Guid id);
         TagModel GetTagByName(string name);
+        int CountTags();
+        TagModel[] GetAllTags();
         (TagModel[], int) GetTags(int offset, int limit);
         public (TagModel[], int) SearchTagsByNameAndDescription(int offset, int limit, string name, string description);
         public (TagModel[], int) SearchTagsByNameOrDescription(int offset, int limit, string name, string description);
