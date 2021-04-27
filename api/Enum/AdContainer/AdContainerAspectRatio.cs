8e4192c58bc28af5c42cd55d8f7eba7fa400a232
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-04-27 18:57:25 +0200

    feature/ad-containers -> Enum are now used as string instead of int

--- api/Enum/AdContainer/AdContainerAspectRatio.cs
+++ api/Enum/AdContainer/AdContainerAspectRatio.cs
@@ -1,16 +1,16 @@
 using System.ComponentModel;
 
-namespace api.Models.AdContainer
+namespace api.Enum.AdContainer
 {
     public enum AdContainerAspectRatio
     {
         [Description("1x1")]
-        Type1X1,
+        Aspect1X1,
 
         [Description("4x3")]
-        Type4X3,
+        Aspect4X3,
 
         [Description("16x9")]
-        Type16X9
+        Aspect16X9
     }
 }
