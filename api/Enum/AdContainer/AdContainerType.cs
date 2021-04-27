d22301c90770a945f6b7b375cf6f25c47c7d154b
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-04-27 16:59:06 +0200

    feature/ad-containers -> AdContainerModel + Updating TagModel + Enums

---
+++ api/Enum/AdContainer/AdContainerType.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel;
+
+namespace api.Models.AdContainer
+{
+    public enum AdContainerType
+    {
+        [Description("2D")]
+        Type2D,
+
+        [Description("3D")]
+        Type3D
+    }
+}
