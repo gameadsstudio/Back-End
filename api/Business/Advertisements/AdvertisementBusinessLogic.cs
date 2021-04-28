976d87c6d019dab6ac0b18664dcd376546849c76
Teo Oriol <teo.oriol@epitech.eu>
2021-04-28 04:15:27 +0200

    feature/ads -> Added business logic

--- api/Business/Advertisements/AdvertisementBusinessLogic.cs
+++ api/Business/Advertisements/AdvertisementBusinessLogic.cs
@@ -1,34 +1,78 @@
+using System.Net;
 using System.Security.Claims;
+using api.Contexts;
+using api.Errors;
 using api.Helpers;
 using api.Models.Advertisement;
+using api.Repositories.Advertisement;
+using AutoMapper;
 
 namespace api.Business.Advertisements
 {
     public class AdvertisementBusinessLogic : IAdvertisementBusinessLogic
     {
-        public AdvertisementModel GetAdvertisementById(string id, Claim currentUser)
+        private readonly IMapper _mapper;
+        private readonly IAdvertisementRepository _repository;
+
+        public AdvertisementBusinessLogic(ApiContext context, IMapper mapper)
         {
-            throw new System.NotImplementedException();
+            _repository = new AdvertisementRepository(context);
+            _mapper = mapper;
         }
 
-        public (int page, int pageSize, int maxPage, AdvertisementModel[] advertisements) GetAdvertisements(PagingDto paging)
+        public AdvertisementModel GetAdvertisementById(string id, Claim currentUser)
         {
-            throw new System.NotImplementedException();
+            var advertisement = _repository.GetAdvertisementById(GuidHelper.StringToGuidConverter(id));
+
+            if (advertisement == null)
+            {
+                throw new ApiError(HttpStatusCode.NotFound, $"Couldn't find advertisement with Id: {id}");
+            }
+
+            return advertisement;
+        }
+
+        public (int page, int pageSize, int maxPage, AdvertisementModel[] advertisements) GetAdvertisements(
+            PagingDto paging)
+        {
+            paging = PagingHelper.Check(paging);
+            var maxPage = _repository.CountAdvertisements() / paging.PageSize + 1;
+            var advertisements = _repository.GetAdvertisements((paging.Page - 1) * paging.PageSize, paging.PageSize);
+            return (paging.Page, paging.PageSize, maxPage, advertisements);
         }
 
         public AdvertisementModel AddNewAdvertisement(AdvertisementCreationModel newAdvertisement)
         {
-            throw new System.NotImplementedException();
+            var advertisement = _mapper.Map(newAdvertisement, new AdvertisementModel());
+
+            return _repository.AddNewAdvertisement(advertisement);
         }
 
-        public AdvertisementModel UpdateAdvertisementById(string id, AdvertisementUpdateModel updatedAdvertisement, Claim currentUser)
+        public AdvertisementModel UpdateAdvertisementById(string id, AdvertisementUpdateModel updatedAdvertisement,
+            Claim currentUser)
         {
-            throw new System.NotImplementedException();
+            var advertisement = _repository.GetAdvertisementById(GuidHelper.StringToGuidConverter(id));
+
+            if (advertisement == null)
+            {
+                throw new ApiError(HttpStatusCode.NotFound, $"Couldn't find advertisement with Id: {id}");
+            }
+
+            advertisement = _mapper.Map(updatedAdvertisement, advertisement);
+
+            return _repository.UpdateAdvertisement(advertisement);
         }
 
         public int DeleteAdvertisementById(string id, Claim currentUser)
         {
-            throw new System.NotImplementedException();
+            var advertisement = _repository.GetAdvertisementById(GuidHelper.StringToGuidConverter(id));
+
+            if (advertisement == null)
+            {
+                throw new ApiError(HttpStatusCode.NotFound, $"Couldn't find user with Id: {id}");
+            }
+
+            return _repository.DeleteAdvertisement(advertisement);
         }
     }
 }
