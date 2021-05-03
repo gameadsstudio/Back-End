75eacef7824a0db32ac0d739ebeadff08e24eddf
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-05-03 17:58:29 +0200

    feature/ad-containers -> WIP Attempt at many to many - object cycle error

--- api/Repositories/AdContainer/AdContainerRepository.cs
+++ api/Repositories/AdContainer/AdContainerRepository.cs
@@ -16,6 +16,8 @@ namespace api.Repositories.AdContainer
 
         public AdContainerModel AddNewAdContainer(AdContainerModel adContainer)
         {
+            var tags = adContainer.Tags.Select(tag => _context.Tag.Local.Single(x => x.Id == tag.Id)).ToList();
+            adContainer.Tags = tags;
             _context.AdContainer.Add(adContainer);
             _context.SaveChanges();
             return adContainer;
