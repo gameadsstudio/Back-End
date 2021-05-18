639a708c614bcf77bd4cfce36709912c3fdeced7
Bastien Roumanteau <bastien@r-pur.com>
2021-05-18 10:34:15 +0200

    feature/games -> endpoints post done / getById WIP

---
+++ api/Models/Game/GamePrivateDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace api.Models.Game
+{
+    public class GamePrivateDto : IGameDto
+    {
+        public Guid Id { get; set; }
+
+        public Guid MediaId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTimeOffset DateLaunch { get; set; }
+
+        public DateTimeOffset DateCreation { get; set; }
+
+        public DateTimeOffset DateUpdate { get; set; }
+    }
+}
--- api/Repositories/Game/IGameRepository.cs
+++ api/Repositories/Game/IGameRepository.cs
@@ -1,11 +1,14 @@
-﻿using api.Models.Game;
+﻿using System;
+using api.Models.Game;
 
 namespace api.Repositories.Game
 {
     public interface IGameRepository
     {
-        int AddNewGame(GameModel game);
+        GameModel AddNewGame(GameModel game);
+        public GameModel GetGameById(Guid id);
         int UpdateGame(GameModel updatedGame, GameModel targetGame);
         int DeleteGame(GameModel game);
+        public GameModel GetGameByName(string name);
     }
 }
