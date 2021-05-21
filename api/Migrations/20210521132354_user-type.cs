6e1eb5926b3644a5e4f6f1c666bdc09c3006c97e
Teo Oriol <teo.oriol@epitech.eu>
2021-05-21 15:24:42 +0200

    feature/user-type -> migration

---
+++ api/Migrations/20210521132354_user-type.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class usertype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "type",
+                table: "user",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "user");
+        }
+    }
+}
