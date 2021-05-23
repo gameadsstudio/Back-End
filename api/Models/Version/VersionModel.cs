0acfbb30b9e179d935e0fddbe1822f04e79298db
Teo Oriol <teo.oriol@epitech.eu>
2021-05-23 18:13:37 +0200

    feature/versions -> WIP

---
+++ api/Models/Version/VersionModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using api.Models.Game;
+
+namespace api.Models.Version
+{
+    public class VersionModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        
+        public GameModel Game { get; set; }
+        
+        public string Name { get; set; }
+        
+        public SemVer
+    }
+}
---
+++ api/Models/Version/VersionPublicDto.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace api.Models.Version
+{
+    public class VersionPublicDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        
+        // public GamePublicDto Game { get; set; }
+    }
+}
