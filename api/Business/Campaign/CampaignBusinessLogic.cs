333329aea74f3adb9b3cf0387f0c3023c9811f16
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-06-12 15:08:57 +0200

    feature/campaigns -> update business logic for add, update and delete a campaign

--- api/Business/Campaign/CampaignBusinessLogic.cs
+++ api/Business/Campaign/CampaignBusinessLogic.cs
@@ -1,20 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using api.Configuration;
 using api.Contexts;
 using api.Models.Campaign;
 using api.Models.Organization;
 using api.Repositories.Campaign;
 using Microsoft.Extensions.Options;
+using AutoMapper;
 
 namespace api.Business.Campaign
 {
     public class CampaignBusinessLogic : ICampaignBusinessLogic
     {
+		private readonly IMapper _mapper;
         private readonly ICampaignRepository _repository;
 
-        public CampaignBusinessLogic(ApiContext context, IOptions<AppSettings> appSettings)
+        public CampaignBusinessLogic(ApiContext context, IOptions<AppSettings> appSettings, IMapper mapper)
         {
             _repository = new CampaignRepository(context);
+			_mapper = mapper;
         }
 
         public CampaignModel AddNewCampaign(CampaignCreationModel newCampaign)
@@ -30,24 +34,36 @@ namespace api.Business.Campaign
                 DateEnd = newCampaign.DateEnd,
                 DateCreation = DateTime.Now,
                 DateUpdate = DateTime.Now,
-                // TODO : fetch from org BL then add organization to this model
                 Organization = new OrganizationModel
                 {
                     Id = Guid.Parse(newCampaign.OrganizationId)
                 }
             };
 
-            throw new NotImplementedException();
+			return _repository.AddNewCampaign(campaign);
         }
 
         public CampaignModel UpdateCampaignById(string id, CampaignUpdateModel updatedCampaign)
         {
-            throw new NotImplementedException();
+			var campaignMerge =_mapper.Map<CampaignUpdateModel, CampaignModel>(
+				updatedCampaign,
+				_repository.GetCampaignById(Guid.Parse(id))
+			);
+
+            return _repository.UpdateCampaign(campaignMerge);
         }
 
         public int DeleteCampaignById(string id)
         {
-            throw new NotImplementedException();
+			var campaign = _repository.GetCampaignById(Guid.Parse(id));
+
+			_repository.DeleteCampaign(campaign);
+			return 0;
         }
+
+		public List<CampaignModel> GetOrganizationCampaigns(string id)
+		{
+			return _repository.GetOrganizationCampaigns(Guid.Parse(id));
+		}
     }
 }
