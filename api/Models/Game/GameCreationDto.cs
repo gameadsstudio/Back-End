aebd6d9b3548b7561d684f1d5953dd7705184606
Téo Oriol <teo.oriol@epitech.eu>
2021-06-14 22:13:03 +0200

    feature/games -> changed orgId to Guid in creation dto

--- api/Models/Game/GameCreationDto.cs
+++ api/Models/Game/GameCreationDto.cs
@@ -6,7 +6,7 @@ namespace api.Models.Game
     public class GameCreationDto
     {
         [Required]
-        public string OrgId { get; set; }
+        public Guid OrgId { get; set; }
 
         [Required]
         public Guid MediaId { get; set; }
