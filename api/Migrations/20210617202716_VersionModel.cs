71cd58df72fde20906a2eab7ce2c7ab9cd6a2246
Téo Oriol <teo.oriol@epitech.eu>
2021-06-17 22:27:24 +0200

    feature/versions -> migration

---
+++ api/Migrations/20210617202716_VersionModel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class VersionModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_game_organization_org_id",
+                table: "game");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "private_email",
+                table: "organization",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "organization",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "org_id",
+                table: "game",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.CreateTable(
+                name: "version",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    game_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    sem_ver = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_version", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_version_game_game_id",
+                        column: x => x.game_id,
+                        principalTable: "game",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_version_game_id",
+                table: "version",
+                column: "game_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_game_organization_org_id",
+                table: "game",
+                column: "org_id",
+                principalTable: "organization",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_game_organization_org_id",
+                table: "game");
+
+            migrationBuilder.DropTable(
+                name: "version");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "private_email",
+                table: "organization",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "organization",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "org_id",
+                table: "game",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_game_organization_org_id",
+                table: "game",
+                column: "org_id",
+                principalTable: "organization",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
