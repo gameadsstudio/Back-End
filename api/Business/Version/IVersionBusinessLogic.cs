52cc96e2ed50c5577d6486756852f6cf4d04b4df
Téo Oriol <teo.oriol@epitech.eu>
2021-06-17 22:26:00 +0200

    feature/versions -> fixed small issues from the pr

--- api/Business/Version/IVersionBusinessLogic.cs
+++ api/Business/Version/IVersionBusinessLogic.cs
@@ -8,12 +8,13 @@ namespace api.Business.Version
     public interface IVersionBusinessLogic
     {
         VersionPublicDto GetVersionById(string id, ConnectedUser currentUser);
-        (int page, int pageSize, int maxPage, IList<VersionPublicDto>) GetVersions(PagingDto paging, VersionFiltersDto
-            filters, ConnectedUser currentUser);
+
+        (int page, int pageSize, int maxPage, IList<VersionPublicDto>) GetVersions(PagingDto paging,
+            VersionFiltersDto filters, ConnectedUser currentUser);
 
         VersionPublicDto AddNewVersion(VersionCreationDto newVersion, ConnectedUser currentUser);
         VersionPublicDto UpdateVersionById(string id, VersionUpdateDto updatedVersion, ConnectedUser currentUser);
-        int DeleteVersionById(string id, ConnectedUser currentUser);
+        void DeleteVersionById(string id, ConnectedUser currentUser);
         VersionModel GetVersionModelById(Guid id);
     }
 }
