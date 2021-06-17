57052a242174f85a9a0feff1519b07f61902bb6a
Téo Oriol <teo.oriol@epitech.eu>
2021-06-17 16:29:00 +0200

    feature/versions -> version update dto

--- api/Models/Version/VersionUpdateDto.cs
+++ api/Models/Version/VersionUpdateDto.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace api.Models.Version
 {
     public class VersionUpdateDto
     {
         public string Name { get; set; }
+        
+        [RegularExpression("^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$")]
+        public string SemVer { get; set; }
     }
 }
