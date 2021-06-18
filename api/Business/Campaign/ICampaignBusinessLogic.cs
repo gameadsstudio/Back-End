93206e07f1955237ec511bcd8a8552bdfbd94a8f
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-06-18 19:39:31 +0200

    features/campaigns -> update business to retrieve a campaign from an identifier

--- api/Business/Campaign/ICampaignBusinessLogic.cs
+++ api/Business/Campaign/ICampaignBusinessLogic.cs
@@ -8,6 +8,7 @@ namespace api.Business.Campaign
         CampaignModel AddNewCampaign(CampaignCreationModel newCampaign);
         CampaignModel UpdateCampaignById(string id, CampaignUpdateModel updatedCampaign);
         int DeleteCampaignById(string id);
+		CampaignModel GetCampaignById(string id);
 		IList<CampaignModel> GetOrganizationCampaigns(string id);
     }
 }
