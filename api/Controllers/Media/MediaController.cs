eb32442924396a64b69a689015237469bf73dee3
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-06-20 21:06:30 +0200

    feature/medias -> Updating media controller

--- api/Controllers/Media/MediaController.cs
+++ api/Controllers/Media/MediaController.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using api.Business.Media;
 using api.Helpers;
 using api.Models.Common;
 using api.Models.Media;
@@ -10,28 +12,38 @@ namespace api.Controllers.Media
     [Route("/v1/medias")]
     public class MediaController : ControllerBase
     {
-        public MediaController()
+        private readonly IMediaBusinessLogic _business;
+
+        public MediaController(IMediaBusinessLogic business)
         {
+            _business = business;
         }
 
         [HttpPost]
         [Consumes("application/x-www-form-urlencoded")]
         public ActionResult<GetDto<MediaPublicDto>> Post([FromForm] MediaCreationDto newMedia)
         {
-            return Created("","");
+            var currentUser = new ConnectedUser(User.Claims);
+
+            return Created("medias",new GetDto<MediaPublicDto>(_business.AddNewMedia(newMedia, currentUser)));
         }
 
         [HttpGet]
         public ActionResult<GetAllDto<MediaPublicDto>> GetAll(
-            [FromQuery] PagingDto paging)
+            [FromQuery] PagingDto paging,
+            [FromQuery] [Required] string versionId)
         {
-            return Ok();
+            var currentUser = new ConnectedUser(User.Claims);
+
+            return Ok(new GetAllDto<MediaPublicDto>(_business.GetMedias(paging, versionId, currentUser)));
         }
 
         [HttpGet("{id}")]
         public ActionResult<GetDto<MediaPublicDto>> Get(string id)
         {
-            return Ok();
+            var currentUser = new ConnectedUser(User.Claims);
+
+            return Ok(new GetDto<MediaPublicDto>(_business.GetMediaById(id, currentUser)));
         }
 
         // Todo : Find a better type for the object, matching engine dtos
@@ -39,18 +51,25 @@ namespace api.Controllers.Media
         [HttpGet("{id}/{engine}")]
         public ActionResult<GetDto<MediaPublicDto>> GetEngine(string id, string engine)
         {
-            return Ok();
+            var currentUser = new ConnectedUser(User.Claims);
+
+            return Ok(new GetDto<MediaPublicDto>(_business.GetEngineMediaById(id, currentUser, engine)));
         }
 
         [HttpPatch("{id}")]
-        public ActionResult<GetDto<MediaPublicDto>> Patch(string id)
+        public ActionResult<GetDto<MediaPublicDto>> Patch(string id,
+            [FromForm] MediaUpdateDto updateDto)
         {
-            return Ok();
+            var currentUser = new ConnectedUser(User.Claims);
+
+            return Ok(new GetDto<MediaPublicDto>(_business.UpdateMediaById(id, updateDto, currentUser)));
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(string id)
         {
+            var currentUser = new ConnectedUser(User.Claims);
+            _business.DeleteMediaById(id, currentUser);
             return NoContent();
         }
     }
