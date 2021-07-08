d705831c23b0c0211e8f76603bace4f0ef041246
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-07-08 22:34:40 +0200

    feature/campaigns -> replace required to bind required in campaign model

--- api/Models/Campaign/CampaignModel.cs
+++ api/Models/Campaign/CampaignModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using api.Models.Organization;
 
 namespace api.Models.Campaign
@@ -31,7 +32,7 @@ namespace api.Models.Campaign
 
         public DateTimeOffset DateUpdate { get; set; }
 
-        [Required]
+        [BindRequired]
         [ForeignKey("OrgId")]
         public OrganizationModel Organization { get; set; }
     }
