09257b257dc057f1368e10dc7dd9152fcd32e45e
Téo Oriol <teo.oriol@epitech.eu>
2021-07-13 16:45:10 +0200

    feature/campaigns -> replace required to bind required

---
+++ api/Models/Campaign/CampaignCreationDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace api.Models.Campaign
+{
+    public class CampaignCreationDto
+    {
+        [BindRequired]
+        public Guid OrganizationId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string AgeMin { get; set; }
+
+        public string AgeMax { get; set; }
+
+        public string Type { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTimeOffset DateBegin { get; set; }
+
+        public DateTimeOffset DateEnd { get; set; }
+    }
+}
