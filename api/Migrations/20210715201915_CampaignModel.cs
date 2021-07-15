d2310b23e2acf26f1e01cfa130ccebdb104639a0
Téo Oriol <teo.oriol@epitech.eu>
2021-07-15 22:19:26 +0200

    feature/campaigns -> migrations

---
+++ api/Migrations/20210715201915_CampaignModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class CampaignModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_campaign_organization_org_id",
+                table: "campaign");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "org_id",
+                table: "campaign",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_campaign_organization_org_id",
+                table: "campaign",
+                column: "org_id",
+                principalTable: "organization",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_campaign_organization_org_id",
+                table: "campaign");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "org_id",
+                table: "campaign",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_campaign_organization_org_id",
+                table: "campaign",
+                column: "org_id",
+                principalTable: "organization",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
