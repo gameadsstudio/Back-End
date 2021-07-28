e5af302c0467b16773b87bd0d2dcf06d8ae74a58
Téo Oriol <teo.oriol@epitech.eu>
2021-07-28 21:56:44 +0200

    fix/advertisements -> updated creation dto

--- api/Models/Advertisement/AdvertisementCreationDto.cs
+++ api/Models/Advertisement/AdvertisementCreationDto.cs
@@ -1,9 +1,17 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace api.Models.Advertisement
 {
     public class AdvertisementCreationDto
     {
+        [BindRequired]
+        public Guid OrganizationId { get; set; }
+        
+        public IList<string> TagNames { get; set; }
+        
         [Required, RangeAttribute(0, 120)]
         public int AgeMin { get; set; }
         
