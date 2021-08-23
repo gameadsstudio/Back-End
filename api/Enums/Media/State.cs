bba4be16130e1d3fd0fc63355c6e96b5e871a899
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2021-08-23 16:46:37 +0200

    feature/medias -> media state

---
+++ api/Enums/Media/State.cs
@@ -0,0 +1,10 @@
+namespace api.Enums.Media
+{
+    public enum MediaStateEnum
+    {
+        Pending = 0,
+        Processing,
+        Processed,
+        Invalid
+    }
+}
--- api/Models/Media/Engine/Unity/MediaUnityPublicDto.cs
+++ api/Models/Media/Engine/Unity/MediaUnityPublicDto.cs
@@ -1,9 +1,14 @@
 using System;
+using api.Enums.Media;
 
 namespace api.Models.Media.Engine.Unity
 {
     public class MediaUnityPublicDto
     {
         public Uri AssetBundleLink { get; set; }
+
+        public MediaStateEnum State { get; set; }
+
+        public string StateMessage { get; set; }
     }
 }
