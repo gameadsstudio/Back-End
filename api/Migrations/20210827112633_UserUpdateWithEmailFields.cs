bc19a3454862de8af1042308fd0f11dc2db56645
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-08-27 17:31:53 +0200

    features/mails -> Add a new migration for the database

---
+++ api/Migrations/20210827112633_UserUpdateWithEmailFields.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class UserUpdateWithEmailFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "email_validated",
+                table: "user",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "email_validated_id",
+                table: "user",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email_validated",
+                table: "user");
+
+            migrationBuilder.DropColumn(
+                name: "email_validated_id",
+                table: "user");
+        }
+    }
+}
