84123ee0732d56ff6bf1275ab7775b0c119eaa62
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-09-16 16:08:54 +0200

    feature/blog -> Remove unused namespace

--- api/Models/Post/PostFiltersDto.cs
+++ api/Models/Post/PostFiltersDto.cs
@@ -1,6 +1,3 @@
-using System;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-
 namespace api.Models.Post
 {
     public class PostFiltersDto
--- api/Models/Post/PostUpdateDto.cs
+++ api/Models/Post/PostUpdateDto.cs
@@ -1,5 +1,4 @@
 using System;
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace api.Models.Post
