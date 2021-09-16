5fe101eb192497e59d67f2e198bbf592f1b6fa10
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-09-16 15:08:15 +0200

    feature/blog -> Add entity framework migration

---
+++ api/Migrations/20210828142716_Blog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class Blog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "blog",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    category = table.Column<string>(type: "text", nullable: true),
+                    content = table.Column<string>(type: "text", nullable: true),
+                    date_creation = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    date_update = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_blog", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "blog");
+        }
+    }
+}
