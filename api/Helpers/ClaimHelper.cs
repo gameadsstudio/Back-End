69777cdfd77f20886811c5399e271b48ffb42079
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-09-18 16:50:59 +0200

    feature/mails -> Add a member to check if an email is validated or not in the user claims helper

--- api/Helpers/ClaimHelper.cs
+++ api/Helpers/ClaimHelper.cs
@@ -11,14 +11,34 @@ namespace api.Helpers
     public class ConnectedUser
     {
         public Guid Id { get; }
+
         public UserRole Role { get; }
+
+        public bool EmailValidated { get; }
 
         public ConnectedUser(IEnumerable<Claim> claims)
         {
-            Id = Guid.Parse(claims.FirstOrDefault(p => p.Type == ClaimTypes.NameIdentifier)?.Value ??
-                            throw new ApiError(HttpStatusCode.BadRequest, "Bad Token - No ID specified in token"));
-            Role = Enum.Parse<UserRole>(claims.FirstOrDefault(p => p.Type == ClaimTypes.Role)?.Value ??
-                                        throw new ApiError(HttpStatusCode.BadRequest, "Bad Token - No role specified in token"));
+            Id = Guid.Parse(
+                claims.FirstOrDefault(
+                    p => p.Type == ClaimTypes.NameIdentifier
+                )?.Value ?? throw new ApiError(
+                    HttpStatusCode.BadRequest,
+                    "Bad Token - No ID specified in token"
+                )
+            );
+            Role = Enum.Parse<UserRole>(
+                claims.FirstOrDefault(
+                    p => p.Type == ClaimTypes.Role
+                )?.Value ?? throw new ApiError(
+                    HttpStatusCode.BadRequest,
+                    "Bad Token - No role specified in token"
+                )
+            );
+            EmailValidated = Boolean.Parse(
+                claims.FirstOrDefault(
+                    p => p.Type == "emailvalidated"
+                )?.Value ?? "false"
+            );
         }
     }
 }
