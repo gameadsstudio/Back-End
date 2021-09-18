da7fde613a1c389ccb5972392a78707ec2fa97dc
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-09-18 16:54:01 +0200

    feature/mails -> Add a middleware for check if a user account is validated or not

---
+++ api/Middlewares/UserAccountValidatedMiddleware.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using api.Attributes;
+using api.Helpers;
+using api.Errors;
+
+namespace api.Middlewares
+{
+    public class UserAccountValidatedMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        private Endpoint _endpoint = null;
+
+        private UserAccountValidatedAttribute _attribute = null;
+
+        public UserAccountValidatedMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            ConnectedUser user = null;
+
+            this._endpoint = context.Features.Get<IEndpointFeature>()?.Endpoint;
+            this._attribute = this._endpoint
+                ?.Metadata
+                .GetMetadata<UserAccountValidatedAttribute>();
+            if (this._attribute == null) {
+                await this._next(context);
+                return;
+            }
+            user = new ConnectedUser(context.User.Claims);
+            if (!user.EmailValidated) {
+                throw new ApiError(
+                    HttpStatusCode.BadRequest,
+                    "Email address not validated"
+                );
+            }
+            await this._next(context);
+        }
+    }
+}
