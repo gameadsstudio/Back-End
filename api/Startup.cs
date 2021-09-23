b216ce1e664fffdea4c4b2bc5a0e6f8839ca4946
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-09-23 17:15:32 +0200

    feature/mails -> Remove an import

--- api/Startup.cs
+++ api/Startup.cs
@@ -16,7 +16,6 @@ using api.Business.Campaign;
 using api.Business.MediaQuery;
 using api.Business.Post;
 using api.Business.Mail;
-using api.Configuration;
 using api.Contexts;
 using api.Enums.User;
 using api.Helpers;
