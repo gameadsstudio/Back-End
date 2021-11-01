90619e03f78128ad3c4a93d60a7b641121efbfaf
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-11-01 16:04:28 +0100

    feature/auth -> Add a business logic class to dependency injection system

--- api/Startup.cs
+++ api/Startup.cs
@@ -113,6 +113,7 @@ namespace api
 
             // Business Logic
             services.AddScoped<IUserBusinessLogic, UserBusinessLogic>();
+			services.AddScoped<IUserAuthServiceBusinessLogic, UserAuthServiceBusinessLogic>();
             services.AddScoped<ITagBusinessLogic, TagBusinessLogic>();
             services.AddScoped<IAdvertisementBusinessLogic, AdvertisementBusinessLogic>();
             services.AddScoped<IOrganizationBusinessLogic, OrganizationBusinessLogic>();
