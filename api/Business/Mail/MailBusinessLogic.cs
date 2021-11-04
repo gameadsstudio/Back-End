b25ccc19eb60fa759a90470884dfd6dcd340c1d3
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-11-04 12:20:21 +0100

    feature/mails -> Resolve some errors

--- api/Business/Mail/MailBusinessLogic.cs
+++ api/Business/Mail/MailBusinessLogic.cs
@@ -10,35 +10,32 @@ namespace api.Business.Mail
         {
         }
 
-        public void send(
-            MailMessage mailMessage
-        )
+        public void send(string to, string subject, string content)
         {
             SmtpClient client = new SmtpClient(
-                Environment.GetEnvironmentVariable(
-                    "GAS_MAIL_HOST"
-                ) ?? "smtp.example.com",
+                Environment.GetEnvironmentVariable("GAS_MAIL_HOST"),
                 Int32.Parse(
-                    Environment.GetEnvironmentVariable(
-                        "GAS_MAIL_PORT"
-                    ) ?? "587"
+                    Environment.GetEnvironmentVariable("GAS_MAIL_PORT")
                 )
             )
             {
                 Credentials = new NetworkCredential(
-                    Environment.GetEnvironmentVariable(
-                        "GAS_MAIL_USERNAME"
-                    ) ?? "smtp_username",
-                    Environment.GetEnvironmentVariable(
-                        "GAS_MAIL_PASSWORD"
-                    ) ?? "smtp_password"
+                    Environment.GetEnvironmentVariable("GAS_MAIL_USERNAME"),
+                    Environment.GetEnvironmentVariable("GAS_MAIL_PASSWORD")
                 ),
                 EnableSsl = bool.Parse(
-                    Environment.GetEnvironmentVariable("GAS_MAIL_SSL") ?? "true"
+                    Environment.GetEnvironmentVariable("GAS_MAIL_SSL")
                 )
             };
 
-            client.Send(mailMessage);
+            client.Send(new MailMessage(
+                Environment.GetEnvironmentVariable(
+                    "GAS_MAIL_ADR_NO_REPLY"
+                ),
+                to,
+                subject,
+                content
+            ));
         }
     }
 }
