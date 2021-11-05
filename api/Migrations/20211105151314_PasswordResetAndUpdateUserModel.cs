08126141529ac23315b286939349e26923319521
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-11-05 16:40:21 +0100

    feature/mails -> Add a migration

---
+++ api/Migrations/20211105151314_PasswordResetAndUpdateUserModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class PasswordResetAndUpdateUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "blog");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "password_reset_id",
+                table: "user",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "post",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    category = table.Column<string>(type: "text", nullable: true),
+                    content = table.Column<string>(type: "text", nullable: true),
+                    date_creation = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    date_update = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_post", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "post");
+
+            migrationBuilder.DropColumn(
+                name: "password_reset_id",
+                table: "user");
+
+            migrationBuilder.CreateTable(
+                name: "blog",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    category = table.Column<string>(type: "text", nullable: true),
+                    content = table.Column<string>(type: "text", nullable: true),
+                    date_creation = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    date_update = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_blog", x => x.id);
+                });
+        }
+    }
+}
