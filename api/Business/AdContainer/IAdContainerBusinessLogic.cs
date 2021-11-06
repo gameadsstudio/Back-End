239a2b068f0016e85323f64f133bcf30e20648fb
Téo Oriol <teo.oriol@epitech.eu>
2021-11-06 18:30:49 +0100

    feature/adcontainers -> added filters

--- api/Business/AdContainer/IAdContainerBusinessLogic.cs
+++ api/Business/AdContainer/IAdContainerBusinessLogic.cs
@@ -7,7 +7,7 @@ namespace api.Business.AdContainer
     public interface IAdContainerBusinessLogic
     {
         AdContainerPublicDto GetAdContainerById(string id, ConnectedUser currentUser);
-        (int page, int pageSize, int totalItemCount, List<AdContainerPublicDto> adContainers) GetAdContainers(PagingDto paging, string orgId, ConnectedUser currentUser);
+        (int page, int pageSize, int totalItemCount, List<AdContainerPublicDto> adContainers) GetAdContainers(PagingDto paging, AdContainerFiltersDto filters, ConnectedUser currentUser);
         AdContainerPublicDto AddNewAdContainer(AdContainerCreationDto newAdContainer, ConnectedUser currentUser);
         AdContainerPublicDto UpdateAdContainerById(string id, AdContainerUpdateDto updatedAdContainer,
             ConnectedUser currentUser);
