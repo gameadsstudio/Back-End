d3aaae4abaa91edd74fbb45fd6d661a8398d4f41
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-11-11 18:11:56 +0100

    feature/mails -> Add a password reset endpoint

--- api/Controllers/User/UserController.cs
+++ api/Controllers/User/UserController.cs
@@ -123,6 +123,14 @@ namespace api.Controllers.User
             return Ok();
         }
 
+        [AllowAnonymous]
+        [HttpPost("reset")]
+        public IActionResult Reset([FromForm] UserResetDto resetDto)
+        {
+            _business.ResetPassword(resetDto);
+            return Ok();
+        }
+
         [HttpGet("search/{search}")]
         public ActionResult<GetAllDto<UserPublicDto>> SearchUser(string search, [FromQuery] PagingDto paging)
         {
