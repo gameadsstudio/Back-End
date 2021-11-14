06272539016bb0646ad5c2dfbdfa7948e25f896e
Téo Oriol <teo.oriol@epitech.eu>
2021-11-14 16:20:01 +0100

    feature/blog -> missing migration

---
+++ api/Migrations/20211114151947_blogToPost.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class blogToPost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "blog");
+
+            migrationBuilder.CreateTable(
+                name: "post",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    category = table.Column<string>(type: "text", nullable: true),
+                    content = table.Column<string>(type: "text", nullable: true),
+                    date_creation = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    date_update = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_post", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "post");
+
+            migrationBuilder.CreateTable(
+                name: "blog",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    category = table.Column<string>(type: "text", nullable: true),
+                    content = table.Column<string>(type: "text", nullable: true),
+                    date_creation = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    date_update = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_blog", x => x.id);
+                });
+        }
+    }
+}
