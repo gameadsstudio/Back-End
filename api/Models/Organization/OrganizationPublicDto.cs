4a39c9f1af2476f115d192409c1c02e26a7e29a1
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-11-25 20:05:53 +0100

    feature/stripe -> remove spaces

--- api/Models/Organization/OrganizationPublicDto.cs
+++ api/Models/Organization/OrganizationPublicDto.cs
@@ -5,13 +5,13 @@ namespace api.Models.Organization
     public class OrganizationPublicDto : IOrganizationDto
     {
         public Guid Id { get; set; }
-        
+
         public string Name { get; set; }
-        
+
         public string Email { get; set; }
 
         public string LogoUrl { get; set; }
-        
+
         public string WebsiteUrl { get; set; }
     }
 }
