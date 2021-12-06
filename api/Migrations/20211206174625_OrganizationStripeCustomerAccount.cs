cbb95c0ff7d851a790640bdcba00e6785c878525
Cédric Hennequin <niquenen@users.noreply.github.com>
2021-12-06 18:46:46 +0100

    feature/stripe -> Add a migration

---
+++ api/Migrations/20211206174625_OrganizationStripeCustomerAccount.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class OrganizationStripeCustomerAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "money",
+                table: "organization",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<string>(
+                name: "stripe_account",
+                table: "organization",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "money",
+                table: "organization");
+
+            migrationBuilder.DropColumn(
+                name: "stripe_account",
+                table: "organization");
+        }
+    }
+}
