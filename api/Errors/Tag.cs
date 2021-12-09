ddd86e6c4b50fa83c271bf24f86c1709eb7543d5
Téo Oriol <teo.oriol@epitech.eu>
2021-12-09 02:02:43 +0100

    feature/errors -> tags

---
+++ api/Errors/Tag.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+
+namespace api.Errors
+{
+    public class TagNotFoundError : ApiError
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string Error { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Detail { get; set; }
+
+        public TagNotFoundError(string detail = "")
+        {
+            StatusCode = HttpStatusCode.NotFound;
+            Error = "tag-0001";
+            ErrorMessage = "Tag not found";
+            Detail = detail;
+        }
+    }
+    
+    public class TagNameAlreadyExist : ApiError
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string Error { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Detail { get; set; }
+
+        public TagNameAlreadyExist(string detail = "")
+        {
+            StatusCode = HttpStatusCode.Conflict;
+            Error = "tag-0002";
+            ErrorMessage = "Tag name already exist";
+            Detail = detail;
+        }
+    }
+}
