10b2d0587233d3f1f5675025ff73f70892fbf360
Téo Oriol <teo.oriol@epitech.eu>
2021-12-09 01:12:50 +0100

    feature/errors -> v1 of errors

---
+++ api/Errors/Authentication.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace api.Errors
+{
+    public class IncorrectCredentials : ApiError
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string Error { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Detail { get; set; }
+
+        public IncorrectCredentials(string detail)
+        {
+            this.StatusCode = HttpStatusCode.Forbidden;
+            this.Error = "auth-0001";
+            this.ErrorMessage = "";
+            this.Detail = detail;
+        }
+    }
+}
