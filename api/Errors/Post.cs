6f8cfacb7218bb39b91bf41d6774d6e2eb1adf02
Téo Oriol <teo.oriol@epitech.eu>
2021-12-09 02:04:36 +0100

    feature/errors -> posts

---
+++ api/Errors/Post.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace api.Errors
+{
+    public class PostNotFoundError : ApiError
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string Error { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Detail { get; set; }
+
+        public PostNotFoundError(string detail = "")
+        {
+            StatusCode = HttpStatusCode.NotFound;
+            Error = "post-0001";
+            ErrorMessage = "Post not found";
+            Detail = detail;
+        }
+    }
+}
